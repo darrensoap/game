4c97364b1ef36132cfb00dd80fe33506f7de0f22
darrensoap <darrensoap@gmail.com>
2017-06-20 16:04:52 +0800

    again

--- frontend/views/UserNumber/index.php
+++ frontend/views/UserNumber/index.php
@@ -10,30 +10,6 @@
   <title>The HTML5 Herald</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
-<<<<<<< HEAD
-  <script>
-    function generateNumber(n){
-      ans = Math.floor(Math.random() * (99 - 0) + 0);
-      //document.write(Math.floor(Math.random() * (99 - 10) + 10));
-      if (n==1) {
-        document.getElementById("result1").innerHTML = ans
-        var ans1;
-        ans = ans1;
-        return showNumber(ans1);
-      }else if (n==2) {
-        document.getElementById("result2").innerHTML = ans
-      }else {
-        document.getElementById("result3").innerHTML = ans
-      }
-    }
-
-    function showNumber(n){
-      console.log(n);
-    }
-  </script>
-=======
-
->>>>>>> a22cbbfa1f45b30fcb18f387d40d0adbe9ecb405
 </head>
 
 <body>
--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -3,8 +3,8 @@
       //document.write(Math.floor(Math.random() * (99 - 10) + 10));
       if (n==1) {
         document.getElementById("result1").innerHTML = ans
-        ans = ans1;
-        return ans1;
+        //ans = ans1;
+        //return ans1;
       }else if (n==2) {
         document.getElementById("result2").innerHTML = ans
       }else {
@@ -12,6 +12,6 @@
       }
     }
 
-    function showNumber(){
+    //function showNumber(){
 
     }
