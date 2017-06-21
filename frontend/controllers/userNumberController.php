849d10d2b07a310441300a62c6d448211b1bac1d
ding94 <dw-ding@hotmail.co.uk>
2017-06-21 11:58:41 +0800

    add all three value to database with userid

--- frontend/controllers/userNumberController.php
+++ frontend/controllers/userNumberController.php
@@ -5,16 +5,25 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use frontend\models\UserNumber;
+use common\models\User;
 
 class UsernumberController extends \yii\web\Controller
 {
     public function actionIndex()
     {
+      
+        if(Yii::$app->user->isGuest)
+        {
+            return $this->redirect(['site/login']);
+        }
         $model = new UserNumber;
         if(Yii::$app->request->isAjax){
             $data = Yii::$app->request->post();
-            $model->fNum = $data['value'];
-            $model->load($data);
+            $model->fNum = $data['fnum'];
+            $model->sNum = $data['snum'];
+            $model->tNum = $data['tnum'];
+            $model->userid = Yii::$app->user->identity->id;
+            $model->isOn = 1;
             $model->save(false);
         }
         return $this->render('index');
